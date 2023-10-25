1bbf19c49ba801b9e79fe483d4f0165ee86998f6
Remy Vila <josephvila123@gmail.com>
2023-10-24 23:21:50 -0400

    debugging cors

--- Integrate-Invigorate/Pages/Auth/Register.js
+++ Integrate-Invigorate/Pages/Auth/Register.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
+import { API_BASE_URL } from '../../env';
+let burl = API_BASE_URL
+
+import axios from 'axios';
+
+
 const RegisterScreen = () => {
   const [formData, setFormData] = useState({
     email: '',
@@ -11,12 +17,34 @@ const RegisterScreen = () => {
 
   const handleRegistration = () => {
     // You can perform registration logic here, e.g., sending the data to your server.
+    axios.post("http://192.168.1.116:8000/register/", formData)
+      .then(response => {
+        // Handle the successful response here.
+        console.log('Registration successful:', response.data);
+      })
+      .catch(error => {
+        // Handle any errors here.
+        console.error('Registration failed:', error);
+        console.log('formData', formData)
+        if (error.response) {
+          // The request was made, but the server responded with an error
+          console.error('Response Data:', error.response.data);
+          console.error('Response Status:', error.response.status);
+          console.error('Response Headers:', error.response.headers);
+        } else if (error.request) {
+          // The request was made, but no response was received (e.g., network error)
+          console.error('Request:', error.request);
+        } else {
+          // Something happened in setting up the request that triggered an error
+          console.error('Error Message:', error.message);
+        }
+    });
     console.log(formData);
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Email</Text>
+      <Text style={styles.label}>Email </Text>
       <TextInput
         style={styles.input}
         onChangeText={(text) => setFormData({ ...formData, email: text })}
@@ -24,7 +52,7 @@ const RegisterScreen = () => {
         keyboardType="email-address"
       />
 
-      <Text style={styles.label}>Username</Text>
+      <Text style={styles.label}>Username </Text>
       <TextInput
         style={styles.input}
         onChangeText={(text) => setFormData({ ...formData, username: text })}
--- Integrate-Invigorate/Pages/HomeScreen.js
+++ Integrate-Invigorate/Pages/HomeScreen.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import axios from 'axios';
 
 const HomeScreen = ({ navigation }) => {
   const goToLogin = () => {
+    // Navigate to the login page (replace 'LoginScreen' with your actual screen name).
     navigation.navigate('LoginScreen');
   };
+
+  const testApi = () => {
+    axios.get('http://192.168.1.116:8000/all_users/')
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(error => {
+        if (error.response) {
+          console.error('Response Data: ', error.response.data)
+          console.error('Response Status:', error.response.status);
+          console.error('Response Headers:', error.response.headers);          
+        } else if (error.request) {
+          console.error('Request: ', error.request);
+        } else {
+          console.error('Error Message: ', error.message);
+        }
+      })
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Integrate Invigorate</Text>
       <Text style={styles.message}>Hello, Healthy Human. Please log in to continue.</Text>
       <Button title="Log In" onPress={goToLogin} />
+      <Button title="test api" onPress={testApi}> test api </Button>
     </View>
   );
 };
